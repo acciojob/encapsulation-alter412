701b5356805a56edc68a8a75193f62cdacd1b0a5
alter412 <139802395+alter412@users.noreply.github.com>
2023-08-12 10:55:00 +0530

    Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        //java: name has private access in com.driver.RWOnly
+//        rw.name = "Name";
+//        System.out.println(rw.name);
+
+        rw.setName("NewName");
+        System.out.println(rw.getName());
+    }
 }
